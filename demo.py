546f98392f7699ecf82116487963e4747da62772
sahilgarg510 <sahilgarg510@gmail.com>
2021-09-03 11:56:48 -0700

    addition of info

--- demo.py
+++ demo.py
@@ -2,3 +2,23 @@ print("git leanring")
 git = 20
 
 print(git)
+print(2+3)
+
+name = "Sahil garg"
+
+print(len(name))
+
+name = ["sahil", "manu"]
+
+
+
+name.append("megha")
+
+print(name[-1])
+
+if (len(name) >= 2) : {
+print("names is greater than 2")
+}
+else :{
+print("no")
+}
